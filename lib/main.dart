da8f6cff10684a60afed7fc84f63b9ee1158518f
huutan3621 <huutan3621@gmail.com>
2021-11-24 16:52:09 +0700

    add filter popup and report page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:ehaba_dating_app/components/navigation_bar.dart';
 import 'package:ehaba_dating_app/screens/Message/Chat/chat_page.dart';
+import 'package:ehaba_dating_app/screens/Notification/report_page.dart';
+import 'package:ehaba_dating_app/screens/ProfilePage/profile_page.dart';
 import 'package:ehaba_dating_app/screens/welcome_page.dart';
 import 'package:flutter/material.dart';
 import 'constraint.dart';
